95cc98e7769fd7992dffbe355bc59c18b3766720
Maksym H <1177472+mordamax@users.noreply.github.com>
2024-08-06 23:23:11 +0100

    Update benchmarking.rs

--- substrate/frame/babe/src/benchmarking.rs
+++ substrate/frame/babe/src/benchmarking.rs
@@ -15,7 +15,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! Benchmarks for the BABE Pallet.
+//! Benchmarks for the BABE Pallet. 123
 
 #![cfg(feature = "runtime-benchmarks")]
 
