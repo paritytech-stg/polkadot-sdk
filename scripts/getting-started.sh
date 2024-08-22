b0865f4a8b9c68a128df1cad763cdc2000ac4572
rzadp <roopert7@gmail.com>
2024-08-22 15:54:20 +0200

    The script

--- scripts/getting-started.sh
+++ scripts/getting-started.sh
@@ -4,7 +4,7 @@ set -e
 
 prompt() {
     while true; do
-        echo "$1 [y/N]"
+        printf "$1 [y/N]\n"
         read yn
         case $yn in
             [Yy]* ) return 0;;  # Yes, return 0 (true)
@@ -17,7 +17,7 @@ prompt() {
 
 prompt_default_yes() {
     while true; do
-        echo "$1 [Y/n]"
+        printf "$1 [Y/n]\n"
         read yn
         case $yn in
             [Yy]* ) return 0;;  # Yes, return 0 (true)
@@ -26,6 +26,17 @@ prompt_default_yes() {
             * ) echo "Please answer yes or no.";;
         esac
     done
+}
+
+clone_and_enter_template() {
+    template="$1" # minimal, solochain, or parachain
+    if [ -d "${template}-template" ]; then
+        printf "\n✅︎ ${template}-template directory already exists. -> Entering.\n"
+    else
+        printf "\n↓ Let's grab the ${template} template from github.\n"
+        git clone --quiet https://github.com/paritytech/polkadot-sdk-${template}-template.git ${template}-template
+    fi
+    cd ${template}-template
 }
 
 cat <<EOF
@@ -39,7 +50,7 @@ cat <<EOF
      |    \__/ |__/| \_/\_/|_/\_/|_/\__/ |_/  |____/|____/|_|\_\ 
                                                                     quickstart!
 
-⚡ We will be setting up an example template and its environment for you to experiment with.
+⚡ We will help setting up the environment for you to experiment with.
 EOF
 
 # Determine OS
@@ -49,9 +60,9 @@ if [ "$os_name" = "Darwin" ]; then
 
     # Check if brew is installed
     if command -v brew >/dev/null 2>&1; then
-        echo "\n✅︎🍺 Homebrew already installed."
+        printf "\n✅︎🍺 Homebrew already installed.\n"
     else
-        if prompt_default_yes "\n🍺 Homebrew is not installed. Install it?"; then
+        if prompt_default_yes "\n🍺 Homebrew is not installed. Install it?\n"; then
             echo "🍺 Installing Homebrew."
             /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
         else
@@ -62,9 +73,9 @@ if [ "$os_name" = "Darwin" ]; then
 
     brew update
     if command -v git >/dev/null 2>&1; then
-        echo "\n✅︎🍺 git already installed."
+        printf "\n✅︎🍺 git already installed.\n"
     else
-        if prompt_default_yes "\n🍺 git seems to be missing but we will need it; install git?"; then
+        if prompt_default_yes "\n🍺 git seems to be missing but we will need it; install git?\n"; then
             brew install git
         else
             echo "❌ Cannot continue without git. Aborting."
@@ -82,24 +93,26 @@ elif [ "$os_name" = "Linux" ]; then
     distro=$( cat /etc/*-release | tr '[:upper:]' '[:lower:]' | grep -Poi '(debian|ubuntu|arch|fedora|opensuse)' | uniq | head -n 1 )
 
     if [ "$distro" = "ubuntu" ]; then
-        echo "\n🐧 Detected Ubuntu. Using apt to install dependencies."
-        sudo apt install --assume-yes git clang curl libssl-dev protobuf-compiler
+        printf "\n🐧 Detected Ubuntu. Using apt to install dependencies.\n"
+        sudo apt -qq update
+        sudo apt -qq install --assume-yes git clang curl libssl-dev protobuf-compiler make
     elif [ "$distro" = "debian" ]; then
-        echo "\n🐧 Detected Debian. Using apt to install dependencies."
-        sudo apt install --assume-yes git clang curl libssl-dev llvm libudev-dev make protobuf-compiler
+        printf "\n🐧 Detected Debian. Using apt to install dependencies.\n"
+        sudo apt -qq update
+        sudo apt -qq install --assume-yes git clang curl libssl-dev llvm libudev-dev make protobuf-compiler
     elif [ "$distro" = "arch" ]; then
-        echo "\n🐧 Detected Arch Linux. Using pacman to install dependencies."
+        printf "\n🐧 Detected Arch Linux. Using pacman to install dependencies.\n"
         pacman -Syu --needed --noconfirm curl git clang make protobuf
     elif [ "$distro" = "fedora" ]; then
-        echo "\n🐧 Detected Fedora. Using dnf to install dependencies."
-        sudo dnf update
-        sudo dnf install clang curl git openssl-devel make protobuf-compiler
+        printf "\n🐧 Detected Fedora. Using dnf to install dependencies.\n"
+        sudo dnf update --assumeyes
+        sudo dnf install --assumeyes clang curl git openssl-devel make protobuf-compiler perl
     elif [ "$distro" = "opensuse" ]; then
-        echo "\n🐧 Detected openSUSE. Using zypper to install dependencies."
-        sudo zypper install clang curl git openssl-devel llvm-devel libudev-devel make protobuf
+        printf "\n🐧 Detected openSUSE. Using zypper to install dependencies.\n"
+        sudo zypper install --no-confirm clang gcc gcc-c++ curl git openssl-devel llvm-devel libudev-devel make awk protobuf-devel
     else
-        if prompt "\n🐧 Unknown Linux distribution. Unable to install dependencies. Continue anyway?"; then
-            echo "\n🐧 Proceeding with unknown linux distribution..."
+        if prompt "\n🐧 Unknown Linux distribution. Unable to install dependencies. Continue anyway?\n"; then
+            printf "\n🐧 Proceeding with unknown linux distribution...\n"
         else
             exit 1
         fi
@@ -110,12 +123,14 @@ else
 fi
 
 # Check if rust is installed
+[ -f "$HOME/.cargo/env" ] && . "$HOME/.cargo/env"
 if command -v rustc >/dev/null 2>&1; then
-    echo "\n✅︎🦀 Rust already installed."
+    printf "\n✅︎🦀 Rust already installed.\n"
 else
     if prompt_default_yes "\n🦀 Rust is not installed. Install it?"; then
         echo "🦀 Installing via rustup."
         curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+        . "$HOME/.cargo/env"
     else
         echo "Aborting."
         exit 1
@@ -131,17 +146,34 @@ if prompt_default_yes "\n🦀 Setup the Rust environment (e.g. WASM support)?"; 
     rustup component add rust-src
 fi
 
-if [ -d "minimal-template" ]; then
-    echo "\n✅︎ minimal-template directory already exists. -> Entering."
-else
-    echo "\n↓ Let's grab the minimal template from github."
-    git clone https://github.com/paritytech/polkadot-sdk-minimal-template.git minimal-template
+if ! prompt "\nWould you like to start with one of the templates?"; then
+    echo "⚡ All done, the environment is ready for hacking."
+    exit 0
 fi
-cd minimal-template
 
-echo "\n⚙️ Let's compile the node."
+while true; do
+    printf "\nWhich template would you like to start with?\n"
+    echo "1) minimal template"
+    echo "2) parachain template"
+    echo "3) solochain template"
+    echo "q) cancel"
+    read -p "#? " template
+    case $template in
+        [1]* ) clone_and_enter_template minimal; break;;
+        [2]* ) clone_and_enter_template parachain; break;;
+        [3]* ) clone_and_enter_template solochain; break;;
+        [qQ]* ) echo "Canceling, not using a template."; exit 0;;
+        * ) echo "Selection not recognized.";;
+    esac
+done
+
+if ! prompt_default_yes "\n⚙️ Let's compile the node? It might take a while."; then
+    echo "⚡ Script finished, you can continue in the ${template}-template directory."
+    exit 0
+fi
+
 cargo build --release
 
-if prompt_default_yes "\n🚀 Everything ready to go, let's run the node?"; then
+if prompt_default_yes "\n🚀 Everything ready to go, let's run the node?\n"; then
     cargo run --release -- --dev
 fi
