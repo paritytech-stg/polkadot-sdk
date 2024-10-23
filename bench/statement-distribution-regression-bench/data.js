e0d2a461320cf727ce6e587bce7919956ecc6bf9
github-action-benchmark <github@users.noreply.github.com>
2024-10-23 12:22:16 +0000

    add statement-distribution-regression-bench (customSmallerIsBetter) benchmark result for b4732add46910370443d092a3f479986060f6df5

---
+++ bench/statement-distribution-regression-bench/data.js
@@ -0,0 +1,52 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1729686135085,
+  "repoUrl": "https://github.com/paritytech-stg/polkadot-sdk",
+  "entries": {
+    "statement-distribution-regression-bench": [
+      {
+        "commit": {
+          "author": {
+            "email": "franciscoaguirreperez@gmail.com",
+            "name": "Francisco Aguirre",
+            "username": "franciscoaguirre"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b4732add46910370443d092a3f479986060f6df5",
+          "message": "Assets in pool with native can be used in `query_weight_to_asset_fee` (#6080)\n\nA follow-up to https://github.com/paritytech/polkadot-sdk/pull/5599.\nAssets in a pool with the native one are returned from\n`query_acceptable_payment_assets`. Now those assets can be used in\n`query_weight_to_asset_fee` to get the correct amount that needs to be\npaid.\n\n---------\n\nCo-authored-by: command-bot <>",
+          "timestamp": "2024-10-22T21:38:38Z",
+          "tree_id": "12b356121059f8349501f3a67754eacd41934345",
+          "url": "https://github.com/paritytech-stg/polkadot-sdk/commit/b4732add46910370443d092a3f479986060f6df5"
+        },
+        "date": 1729686127371,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Sent to peers",
+            "value": 127.95799999999997,
+            "unit": "KiB"
+          },
+          {
+            "name": "Received from peers",
+            "value": 106.39999999999996,
+            "unit": "KiB"
+          },
+          {
+            "name": "statement-distribution",
+            "value": 0.03745166128199999,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.04719612842999992,
+            "unit": "seconds"
+          }
+        ]
+      }
+    ]
+  }
+}
