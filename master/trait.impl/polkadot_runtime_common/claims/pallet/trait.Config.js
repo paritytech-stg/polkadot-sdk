7e879a1465852ce0bd4ac1347040a9e0fc3bafa3
github-action-benchmark <github@users.noreply.github.com>
2025-01-23 17:05:32 +0000

    ___Updated docs

---
+++ master/trait.impl/polkadot_runtime_common/claims/pallet/trait.Config.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["polkadot_test_runtime",[["impl Config for <a class=\"struct\" href=\"polkadot_test_runtime/struct.Runtime.html\" title=\"struct polkadot_test_runtime::Runtime\">Runtime</a>"]]],["rococo_runtime",[["impl Config for <a class=\"struct\" href=\"rococo_runtime/struct.Runtime.html\" title=\"struct rococo_runtime::Runtime\">Runtime</a>"]]],["yet_another_parachain_runtime",[]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[178,158,37]}
